82f16991785c62e857d045bc2a8125f4f5af03f8
bbizz <bbizz@DESKTOP-F1BIQ76>
2024-01-10 12:18:57 -0500

    nw changes

--- router/src/Components/Blue.jsx
+++ router/src/Components/Blue.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
 export default function Blue() {
   return (
     <div className="blue">
       <h1>BlUE</h1>
     </div>
-  )
+  );
 }
--- router/src/Components/Red.jsx
+++ router/src/Components/Red.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function Red() {
   return (
     <div className="red">
