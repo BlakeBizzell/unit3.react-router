159b8e18447ef82b4048940380c7c41d2a2fefd7
bbizz <bbizz@DESKTOP-F1BIQ76>
2024-01-10 12:11:27 -0500

    updates

---
+++ router/src/App.jsx
@@ -0,0 +1,28 @@
+import "./index.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./Components/Red";
+import Blue from "./Components/Blue";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <>
+      <div id="container">
+        <h1>Hello React Router!</h1>
+        <div id="navbar">
+          <Link to="/blue">Blue</Link>
+          <Link to="/red">Red</Link>
+          <Link to="/">Home</Link>
+        </div>
+        <div id="main-section"><Routes> 
+           <Route path="/blue" element={<Blue />} />
+           <Route path="/red" element={<Red />} /> 
+           <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ router/src/Components/Blue.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Blue() {
+  return (
+    <div className="blue">
+      <h1>BlUE</h1>
+    </div>
+  )
+}
